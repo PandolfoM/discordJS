8e7c3221b1432ce2a7bab1f167d23bb58f4f988f
Matthew Pandolfo <matt@pandolfo.com>
2021-10-07 10:42:08 -0400

    ping command

---
+++ src/commands/information/ping.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction, client) {
+    const sent = await interaction.reply({
+      content: "Pinging...",
+      fetchReply: true,
+    });
+    interaction.editReply(
+      `Roundtrip latency: ${
+        sent.createdTimestamp - interaction.createdTimestamp
+      }ms`
+    );
+  },
+};
