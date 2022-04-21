dd8441a126594425d838bc6a5b3ac7284662e294
Matthew Pandolfo <matt@pandolfo.com>
2022-04-20 21:15:22 -0400

    reply to facebook marketplace links

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,6 +3,8 @@ module.exports = {
   execute(message, client) {
     if (message.content.toLowerCase() == "sybau") {
       message.reply("ciaboo*");
+    } else if (message.content.includes("https://www.facebook.com/marketplace")) {
+      message.reply("<:batChest:899088881243283466>")
     }
     // else if (message.content.includes("<@248910149442338816>")) {
     //   message.reply('STFU RETARD + RATIO')
