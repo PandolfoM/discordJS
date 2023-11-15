3f8cb54da1f72f47d48a6369e41ab1118e9b579c
Matthew Pandolfo <matt@pandolfo.com>
2023-11-15 12:00:43 -0500

    response to user mentioning bot

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,8 +1,12 @@
 module.exports = {
   name: "messageCreate",
   execute(message) {
+    console.log(message.content);
     if (message.content.toLowerCase().includes("sybau")) {
       message.reply("ciaboo*");
+    }
+    if (message.content.toLowerCase() === "<@600149573872254976>") {
+      message.reply("What you want bitch");
     }
   },
 };
