6992c7f8a26cb409f45df52acb9eb0f241fa08e5
Matthew Pandolfo <matt@pandolfo.com>
2023-11-16 12:22:30 -0500

    add and remove items from webscraper

---
+++ src/commands/webscraper/removeurl.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const logger = require("../../utils/logger");
+const addUrl = require("../../utils/firebaseUtils");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("removeurl")
+    .setDescription("Remove item from webscraper")
+    .addStringOption((option) =>
+      option.setName("url").setRequired(true).setDescription("AMAZON URLS ONLY")
+    ),
+  async execute(interaction) {
+    const url = interaction.options.getString("url");
+
+    if (
+      url.toLowerCase().includes("amazon.com") ||
+      url.toLowerCase().includes("a.co")
+    ) {
+      try {
+        await addUrl(interaction.user.id, url);
+        await interaction.reply({
+          content: `removed: ${url}`,
+          ephemeral: true,
+        });
+      } catch (error) {
+        logger(error);
+      }
+    } else {
+      try {
+        await interaction.reply({
+          content: "Not a valid url",
+          ephemeral: true,
+        });
+      } catch (error) {
+        logger(error);
+      }
+    }
+  },
+};
--- src/utils/firebaseUtils.js
+++ src/utils/firebaseUtils.js
@@ -1,15 +1,48 @@
-const { query, collection, getDocs } = require("firebase/firestore");
+const {
+  query,
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  arrayUnion,
+  arrayRemove,
+} = require("firebase/firestore");
 const db = require("../firebaseConfig");
+const logger = require("./logger");
 
 async function getUrls() {
   try {
     const q = query(collection(db, "webscraper"));
     const qSnapshot = await getDocs(q);
 
-    return qSnapshot.docs.map((doc) => ({ id: doc.id, urls: doc.data().urls }));
+    return qSnapshot.docs.map((i) => ({ id: i.id, urls: i.data().urls }));
   } catch (error) {
     return;
   }
 }
 
+async function addUrl(userid, url) {
+  try {
+    const ref = doc(db, "webscraper", userid);
+    await updateDoc(ref, {
+      urls: arrayUnion(url),
+    });
+  } catch (error) {
+    logger(error);
+  }
+}
+
+async function removeUrl(userid, url) {
+  try {
+    const ref = doc(db, "webscraper", userid);
+    await updateDoc(ref, {
+      urls: arrayRemove(url),
+    });
+  } catch (error) {
+    logger(error);
+  }
+}
+
 module.exports = getUrls;
+module.exports = addUrl;
+module.exports = removeUrl;
