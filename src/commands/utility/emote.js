e7516263eec2af3f3ce93d6712ae1994f84fac43
Matthew Pandolfo <matt@pandolfo.com>
2024-11-09 10:56:42 -0500

    remove ephemeral on emote steal

--- src/commands/utility/emote.js
+++ src/commands/utility/emote.js
@@ -56,7 +56,7 @@ module.exports = {
               },
             },
           ],
-          ephemeral: true,
+          // ephemeral: true,
         });
       } catch (error) {
         logger(error);
